e1039988002091ca4d06d9a3982a224e774507b4
daniel salgado <daniel.salgado.p@gmail.com>
2021-05-14 00:37:10 +0200

    cp was commented

--- create_test_scaffold.sh
+++ create_test_scaffold.sh
@@ -4,7 +4,7 @@ rm -rf data/*
 mv 10mb_0.mp4 data/
 # TODO cleaner way to create secuencial files
 cd data
-# cp 10mb_0.mp4 10mb_1.mp4
-# cp 10mb_0.mp4 10mb_2.mp4
-# cp 10mb_0.mp4 10mb_3.mp4
-# cd -
+cp 10mb_0.mp4 10mb_1.mp4
+cp 10mb_0.mp4 10mb_2.mp4
+cp 10mb_0.mp4 10mb_3.mp4
+cd -

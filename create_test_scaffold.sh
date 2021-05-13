2da10b6ae9df867fa79364671259919c9599cebb
daniel salgado <daniel.salgado.p@gmail.com>
2021-05-14 00:36:25 +0200

    programatically creating files for test. Renamed 10mb.mp4 to 10mb_0.mp4

---
+++ create_test_scaffold.sh
@@ -0,0 +1,10 @@
+# TODO cleaner way to remove all BUT one file
+mv data/10mb_0.mp4 .
+rm -rf data/*
+mv 10mb_0.mp4 data/
+# TODO cleaner way to create secuencial files
+cd data
+# cp 10mb_0.mp4 10mb_1.mp4
+# cp 10mb_0.mp4 10mb_2.mp4
+# cp 10mb_0.mp4 10mb_3.mp4
+# cd -

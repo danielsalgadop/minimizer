be01198e141d76c3a97054f09137e26b8130ec86
daniel salgado <daniel.salgado.p@gmail.com>
2021-05-22 11:44:09 +0200

    isVideo function uses config/video_extensions.json

--- src/php/minimizer.php
+++ src/php/minimizer.php
@@ -1,15 +1,23 @@
 <?php
 const INPUT_DIR = '../../data';
-$dir_handle = opendir(INPUT_DIR);
+const CONFIG_DIR = '../../config';
+$input_dir_handle = opendir(INPUT_DIR);
+$config_dir_handle = opendir(CONFIG_DIR);
 
-while($file_name = readdir($dir_handle)){
+while($file_name = readdir($input_dir_handle)){
     if(isAVideo($file_name)){
         minimize($file_name);
     }
 }
 
 function isAVideo(string $file_name):bool {
-    return preg_match('/.*.mp4$/',$file_name);
+    $video_extensions = json_decode(file_get_contents(CONFIG_DIR.'/video_extensions.json'));
+    foreach($video_extensions as $video_extension){
+        if(preg_match("/${video_extension}$/", $file_name)){
+            return true;
+        }
+    }
+    return false;
 }
 
 function minimize(string $file_name, $size_ratio = 4):void {
